081b8cf16ee40e2346c74558b0a1c25d8ed79ca4
kaylatillman <kaylatillman319@gmail.com>
2019-08-26 12:30:26 -0700

    Moved Helper functions into their own file

--- controllers/helpers.js
+++ controllers/helpers.js
@@ -1,1 +1,28 @@
 // MOVE ALL SEED FUNCTIONS HERE
+
+
+const generateReview = () => {
+  Review.create({
+    first_name: faker.name.firstName(),
+    profile_picture: faker.image.avatar(),
+    localized_date: faker.date.past(),
+    reviewer: faker.lorem.text(),
+    accuracy: ((faker.random.number() % 5) + 1.0),
+    communication: ((faker.random.number() % 5) + 1.0),
+    cleanliness: ((faker.random.number() % 5) + 1.0),
+    location: ((faker.random.number() % 5) + 1.0),
+    checkin: ((faker.random.number() % 5) + 1.0),
+    value: ((faker.random.number() % 5) + 1.0)
+  })
+};
+
+const generateResponse = () => {
+  Host.create({
+    first_name: 'Micheal',
+    profile_picture: 'https://s3.amazonaws.com/uifaces/faces/twitter/bowbrick/128.jpg',
+    comment: faker.lorem.text(),
+    localized_date: faker.date.recent(),
+    review_id: ((faker.random.number() % 100) + 1.0)
+  })
+};
+
--- db/index.js
+++ db/index.js
@@ -40,42 +40,20 @@ Host.init({
 
 
 //BUILD ASSOCIATIONS
-// Review.belongsTo(Reviewee);
-// Reviewee.hasMany(Review);
+Review.belongsTo(Host);
+Host.hasMany(Reviews);
 
 
-const generateReview = () => {
-  Review.create({
-    first_name: faker.name.firstName(),
-    profile_picture: faker.image.avatar(),
-    localized_date: faker.date.past(),
-    reviewer: faker.lorem.text(),
-    accuracy: ((faker.random.number() % 5) + 1.0),
-    communication: ((faker.random.number() % 5) + 1.0),
-    cleanliness: ((faker.random.number() % 5) + 1.0),
-    location: ((faker.random.number() % 5) + 1.0),
-    checkin: ((faker.random.number() % 5) + 1.0),
-    value: ((faker.random.number() % 5) + 1.0)
-  })
-};
-
-const generateResponse = () => {
-  Host.create({
-    first_name: 'Micheal',
-    profile_picture: 'https://s3.amazonaws.com/uifaces/faces/twitter/bowbrick/128.jpg',
-    comment: faker.lorem.text(),
-    localized_date: faker.date.recent(),
-    review_id: ((faker.random.number() % 100) + 1.0)
-  })
-};
 
 
-//SYNC EACH TABLE TO DATABASE
+
+
+// //SYNC EACH TABLE TO DATABASE
 Review.sync({ force: true })
   .then(() => {
-    for (var i = 0; i < 100; i += 1) {
-      generateReview()
-    }
+    // for (var i = 0; i < 100; i += 1) {
+    //   generateReview()
+    // }
   })
   .catch((err) => {
     console.log(err)
@@ -84,9 +62,9 @@ Review.sync({ force: true })
 
 Host.sync({ force: true })
   .then(() => {
-    for (var j = 0; j < 20; j += 1) {
-      generateResponse()
-    }
+    // for (var j = 0; j < 20; j += 1) {
+    //   generateResponse()
+    // }
   })
   .catch((err) => {
     console.log(err)
